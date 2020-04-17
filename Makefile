1b866fddcbe4331ee9206a63688ef6127950e48f
ckping3145 <chenkp1990@163.com>
2020-04-17 16:40:59 +0800

    first

---
+++ Makefile
@@ -0,0 +1,31 @@
+
+CFLAGS := -g -O2 -W -Wall -m32
+OBJDIR := ../../bin/
+SRCS := $(shell find . -name '*.c*')
+OBJSTMP := $(filter %.o, ${SRCS:.cpp=.o})
+OBJS := $(shell echo "${OBJSTMP}"|sed 's/\.\//\.\.\/\.\.\/bin\//g')
+CC = g++
+
+TARGET = ts
+
+.PHONY: all clean
+
+all: ${TARGET}
+
+$(TARGET):	$(OBJS)
+	@echo "Generating Target: $(@)"
+	@echo "Invoking C++ Linker"
+# $@ 目标, $^ 输入列表 $< 第一个输入
+	$(CC) -o $(@) $(^) $(CFLAGS)  
+	@echo "Finishd Building Target: $(@)"
+	@echo ' '
+
+${OBJDIR}%.o:	%.cpp
+	@echo "Building file: $(<)"  #第一个输入
+	@echo "Invoking C++ Compiler"
+	$(CC) -c -o $(@) $(<) $(CFLAGS)
+	@echo "Finishd Building $(<)"
+	@echo ' '
+
+clean:
+	-rm -rf ${OBJS} ${TARGET}
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include "../../include/utils.h"
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+    if (argc == 1)
+    {
+        cout << "Usage : << argv[0] << <file path>" << endl;
+        return -1;
+    }
+    char *filePath = argv[1];
+    unsigned short pktpid = 66; 
+
+    int fileNameLen = strlen(filePath);
+    if (fileNameLen <= 3)
+    {
+        cout << "File path length not correct. need *.ts file" << endl;
+        return -1;
+    }
+
+    if (0 != strncmp(".ts", filePath+fileNameLen-3, 3))
+    {
+        cout << "File path type not correct. need *.ts file" << endl;
+        return -1;
+    }
+
+    if (argc >= 3)
+    {
+        pktpid = atoi(argv[2]);
+        cout << "Got Packet Pid " << pktpid << "." << endl;
+    }
+
+    cout << "App Start" << endl << endl;
+
+    //read_pkts_haed_info(TestFileName);
+    //parse_pkts_data(filePath, pktpid);
+
+    cout << endl << "App ends" << endl;
+    return 0;
+}
+
+
